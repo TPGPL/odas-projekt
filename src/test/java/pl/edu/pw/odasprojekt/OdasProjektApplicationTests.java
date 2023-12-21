b44287145b00908be6d7336d2605683e445cddcd
TPGPL <TPGPL@users.noreply.github.com>
2023-12-21 16:58:08 +0100

    Initial commit

---
+++ src/test/java/pl/edu/pw/odasprojekt/OdasProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.odasprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdasProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
