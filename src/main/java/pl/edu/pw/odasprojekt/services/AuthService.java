2e3bf161523445222c8a0731d1f676e065243036
TPGPL <TPGPL@users.noreply.github.com>
2023-12-23 18:35:26 +0100

    Add login backend

--- src/main/java/pl/edu/pw/odasprojekt/services/AuthService.java
+++ src/main/java/pl/edu/pw/odasprojekt/services/AuthService.java
@@ -1,7 +1,32 @@
 package pl.edu.pw.odasprojekt.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.edu.pw.odasprojekt.model.domain.UserAuth;
+import pl.edu.pw.odasprojekt.model.dtos.PasswordFragmentDto;
+import pl.edu.pw.odasprojekt.repositories.AuthRepository;
+
+import java.util.HashMap;
 
 @Service
 public class AuthService {
+    private final AuthRepository authRepository;
+
+    @Autowired
+    public AuthService(AuthRepository authRepository) {
+        this.authRepository = authRepository;
+    }
+
+    public int calculateLoginSecret(int userId, PasswordFragmentDto[] passFrags) {
+        int[] indices = {passFrags[0].getIndex() + 1, passFrags[1].getIndex() + 1, passFrags[2].getIndex() + 1};
+        HashMap<Integer, UserAuth> secrets = new HashMap<>();
+
+        for (int id : indices) {
+            secrets.put(id, authRepository.findByIndexAndUserId(indices[0], userId).orElse(null));
+        }
+
+        return (secrets.get(indices[0]).getSecret() + passFrags[0].getValue()) * ((indices[1] * indices[2]) / ((indices[0] - indices[1]) * (indices[0] - indices[2])))
+                + (secrets.get(indices[1]).getSecret() + passFrags[1].getValue()) * ((indices[0] * indices[2]) / ((indices[1] - indices[0]) * (indices[1] - indices[2])))
+                + (secrets.get(indices[2]).getSecret() + passFrags[2].getValue()) * ((indices[0] * indices[1]) / ((indices[2] - indices[1]) * (indices[2] - indices[0])));
+    }
 }
--- src/main/java/pl/edu/pw/odasprojekt/services/UserService.java
+++ src/main/java/pl/edu/pw/odasprojekt/services/UserService.java
@@ -1,17 +1,83 @@
 package pl.edu.pw.odasprojekt.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.edu.pw.odasprojekt.model.ServiceResponse;
 import pl.edu.pw.odasprojekt.model.domain.UserData;
+import pl.edu.pw.odasprojekt.model.dtos.PasswordFragmentDto;
+import pl.edu.pw.odasprojekt.model.dtos.UserLoginDto;
 import pl.edu.pw.odasprojekt.repositories.UserRepository;
+
+import java.util.regex.Pattern;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final AuthService authService;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, JwtService jwtService, AuthService authService, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.jwtService = jwtService;
+        this.authService = authService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public UserData getUserByClientNumber(String clientNumber) {
+        return userRepository.findByClientNumber(clientNumber).orElse(null);
+    }
+
+    public ServiceResponse<String> login(UserLoginDto dto) {
+        if (dto == null || !verifyClientNumber(dto.getClientNumber()) || !verifyPasswordFragments(dto.getPasswordFrags())) {
+            return ServiceResponse.<String>builder().success(false).build();
+        }
+
+        var user = getUserByClientNumber(dto.getClientNumber());
+        int calculatedSecret = authService.calculateLoginSecret(user.getId(), dto.getPasswordFrags());
+
+        var hash = passwordEncoder.encode(calculatedSecret + user.getSecretSalt());
+
+        if (hash.equals(user.getSecretHash())) { // login success
+            return ServiceResponse.<String>builder().success(true).data(jwtService.generateJwtForUser(dto.getClientNumber())).build();
+        }
+
+        return ServiceResponse.<String>builder().success(false).build();
+    }
+
+    private boolean verifyPasswordFragments(PasswordFragmentDto[] passwordFrags) {
+        if (passwordFrags == null) {
+            return false;
+        }
+
+        var pattern = Pattern.compile("[0-9a-zA-Z@!$%]");
+
+        for (var frag : passwordFrags) {
+            if (frag == null)
+                return false;
+
+            if (frag.getIndex() < 0 || frag.getIndex() > 15)
+                return false;
+
+            if (!pattern.matcher(String.valueOf(frag.getValue())).find()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean verifyClientNumber(String clientNumber) {
+        if (clientNumber == null || clientNumber.length() != 13) {
+            return false;
+        }
+
+        if (!Pattern.compile("[0-9]{13}").matcher(clientNumber).find()) {
+            return false;
+        }
+
+        return userRepository.existsByClientNumber(clientNumber);
     }
 }
