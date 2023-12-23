8359c3a40595df354545602ff772f3e51c7ace47
TPGPL <TPGPL@users.noreply.github.com>
2023-12-23 16:41:54 +0100

    Add constructors to service response

--- src/main/java/pl/edu/pw/odasprojekt/model/ServiceResponse.java
+++ src/main/java/pl/edu/pw/odasprojekt/model/ServiceResponse.java
@@ -1,11 +1,15 @@
 package pl.edu.pw.odasprojekt.model;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ServiceResponse<T> {
     private T data;
     private boolean success;
