abeabb02eb8c0dec79216f28fcaf42cc258b6842
TPGPL <TPGPL@users.noreply.github.com>
2023-12-23 01:51:47 +0100

    Add payment dto

---
+++ src/main/java/pl/edu/pw/odasprojekt/model/dtos/PaymentDto.java
@@ -0,0 +1,14 @@
+package pl.edu.pw.odasprojekt.model.dtos;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Builder
+public class PaymentDto {
+    private String title;
+    private Date sentAt;
+    private double amount;
+}
