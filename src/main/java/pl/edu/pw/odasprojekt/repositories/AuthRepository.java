c8dfa9e30d50f203b85a3e4a709b18d5338690d5
TPGPL <TPGPL@users.noreply.github.com>
2023-12-23 01:05:20 +0100

    Add delete method to auth repository

--- src/main/java/pl/edu/pw/odasprojekt/repositories/AuthRepository.java
+++ src/main/java/pl/edu/pw/odasprojekt/repositories/AuthRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface AuthRepository extends CrudRepository<UserAuth, Integer> {
     Optional<UserAuth> findByIndexAndUserId(int index, int id);
+    void deleteAllByUserId(int id);
 }
