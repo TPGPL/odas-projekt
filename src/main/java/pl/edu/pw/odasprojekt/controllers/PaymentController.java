bf625c12067650b07135a40cbadb85023cc4d17c
TPGPL <TPGPL@users.noreply.github.com>
2023-12-24 03:18:18 +0100

    Add payment history view

--- src/main/java/pl/edu/pw/odasprojekt/controllers/PaymentController.java
+++ src/main/java/pl/edu/pw/odasprojekt/controllers/PaymentController.java
@@ -1,18 +1,35 @@
 package pl.edu.pw.odasprojekt.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import pl.edu.pw.odasprojekt.services.PaymentService;
 
 @Controller
 @RequestMapping("/payments")
 public class PaymentController {
+    private final PaymentService service;
+
+    @Autowired
+    public PaymentController(PaymentService service) {
+        this.service = service;
+    }
+
     @RequestMapping(method = RequestMethod.GET)
-    public String paymentsHistory() {
+    public String paymentsHistory(Model model) {
+        var clientNumber = ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
+        var payments = service.getAllPaymentsForClient(clientNumber).getData();
+
+        model.addAttribute("payments", payments);
+
         return "payments/history";
     }
 
-    @RequestMapping(path="/new" ,method = RequestMethod.GET)
+    @RequestMapping(path = "/new", method = RequestMethod.GET)
     public String createPayment() {
         return "payments/new";
     }
