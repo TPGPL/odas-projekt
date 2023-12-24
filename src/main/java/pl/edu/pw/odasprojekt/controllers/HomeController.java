671e75551a02207ea5f5d81a7a0498c1ae4241f9
TPGPL <TPGPL@users.noreply.github.com>
2023-12-24 01:35:37 +0100

    Redirect from home to dashboard if logged

--- src/main/java/pl/edu/pw/odasprojekt/controllers/HomeController.java
+++ src/main/java/pl/edu/pw/odasprojekt/controllers/HomeController.java
@@ -1,15 +1,23 @@
 package pl.edu.pw.odasprojekt.controllers;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
-
-    // TODO: redirect to dashboard (maybe grantedAuthority = ROLE_USER)
+    
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String index() {
+        var auth = SecurityContextHolder.getContext().getAuthentication();
+
+        for (var aut : auth.getAuthorities()) {
+            if (aut.getAuthority().equals("ROLE_USER")) {
+                return "redirect:/dashboard";
+            }
+        }
+
         return "redirect:/auth/login";
     }
 }
