e299f32307dd67084b6d3dd053738530f477064a
TPGPL <TPGPL@users.noreply.github.com>
2024-01-01 20:32:10 +0100

    Setup SQLite DB

--- src/main/java/pl/edu/pw/odasprojekt/model/domain/UserAuth.java
+++ src/main/java/pl/edu/pw/odasprojekt/model/domain/UserAuth.java
@@ -16,6 +16,7 @@ public class UserAuth {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name = "idx")
     private int index;
     private int secret;
     @ManyToOne
