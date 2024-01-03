719a7021aa07a4d02454549521c9c9564f7b2285
TPGPL <TPGPL@users.noreply.github.com>
2024-01-03 10:36:33 +0100

    Use csrf

--- src/main/java/pl/edu/pw/odasprojekt/security/SecurityConfiguration.java
+++ src/main/java/pl/edu/pw/odasprojekt/security/SecurityConfiguration.java
@@ -6,12 +6,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
@@ -19,19 +21,25 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http // TODO: configure further?
-                .csrf(AbstractHttpConfigurer::disable) // TODO: use actual CSRF
+        http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf.csrfTokenRepository(cookieCsrfTokenRepository()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth ->
                         auth
                                 .requestMatchers("/auth/logout").authenticated()
                                 .requestMatchers(AntPathRequestMatcher.antMatcher("/auth/**")).permitAll()
-                                .requestMatchers(AntPathRequestMatcher.antMatcher("/css/**")).permitAll()
                                 .requestMatchers("/").permitAll()
                                 .anyRequest().authenticated());
 
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(AntPathRequestMatcher.antMatcher("/css/**"))
+                .requestMatchers("/favicon.ico");
     }
 
     @Bean
@@ -47,5 +55,13 @@ public class SecurityConfiguration {
     @Bean
     public JwtFilter jwtFilter() {
         return new JwtFilter();
+    }
+
+    @Bean
+    public CookieCsrfTokenRepository cookieCsrfTokenRepository() {
+        //var repo = new CookieCsrfTokenRepository();
+        //repo.setSecure(true); TODO
+
+        return new CookieCsrfTokenRepository();
     }
 }
